c6b2422e0772217099d2d23424277d0335f53296
lucasurbain <lucasurbain68@protonmail.ch>
2024-03-10 13:35:19 +0100

    init

---
+++ Fox'Up/Fox'Up/Fox_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fox_UpApp.swift
+//  Fox'Up
+//
+//  Created by lucas urbain on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Fox_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
